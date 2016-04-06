5959b1a5a2e0b8e263330eca85796f8678633abe
moutoum <maxence.moutoussamy@epitech.eu>
2016-04-06 11:56:14 +0200

    Added files via upload

--- test_non_regression.sh
+++ test_non_regression.sh
@@ -18,7 +18,7 @@ do
   NB_TEST=$(echo $NB_TEST+1 | bc)
   echo -n "$(basename $ELEMENT) -> "
   $ELEMENT
-  diff ./Moulinette/.tmp1 ./Moulinette/.tmp2 >& /dev/null
+  diff .tmp1 .tmp2 >& /dev/null
 
   if [ $? == 0 ];then
     echo -e "\e[32;1mOK\e[0m"
@@ -27,8 +27,8 @@ do
     echo -e "\e[31;1mKO\e[0m"
   fi
 
-  rm -f /Moulinette/.tmp1
-  rm -f /Moulinette/.tmp2
+  rm -f .tmp1
+  rm -f .tmp2
 done
 
 echo -e "\n$NB_VALID / $NB_TEST -> $(echo $NB_VALID*100/$NB_TEST | bc)%"
